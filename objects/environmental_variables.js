6ea52db7fa3f3819b48a953769c6a3746fe650c4
Sahir Shehzad <shehzadss@vcu.edu>
2021-11-21 14:35:02 -0500

    Personal

--- objects/environmental_variables.js
+++ objects/environmental_variables.js
@@ -1,4 +1,4 @@
-export const S3_BUCKET_NAME = 'media-share-v2-resources';
-export const APP_CLIENT_ID = '4cr3dhaql6ck43k02e7ibvtl08';
+export const S3_BUCKET_NAME = 'aws-cognito-resources';
+export const APP_CLIENT_ID = '5l5897hr9aier4cfe9cbv6temp';
 export const APP_WEB_DOMAIN = 'media-management.auth.us-east-1.amazoncognito.com';
-export const REDIRECT_URI = 'https://media-management.s3.amazonaws.com/index.html';
+export const REDIRECT_URI = 'https://shehzadsite.s3.amazonaws.com/index.html';
